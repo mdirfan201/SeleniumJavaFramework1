7bfee9fbf0d03c97b34ab1bfec9f7890815ed4d3
MY-PC <MY-PC@DESKTOP-8EQSD1V>
2020-12-10 12:55:54 +0530

    Commit -Add First project

---
+++ seleniumwebdriver/src/seleniumwebdriver/firstprg.java
@@ -0,0 +1,17 @@
+package seleniumwebdriver;
+
+public class firstprg {
+
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int cv=a*b;
+		//Calendar cal =new Calendar;
+		
+		System.out.println(cv);
+		System.out.println("Hello world...............!= " + cv + ";...........The OutPut is correct.");
+	   
+		
+	}
+}
